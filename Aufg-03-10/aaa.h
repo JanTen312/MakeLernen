3d79659b3e8d3a81020595effd8d8dcc11a37d54
JanTen312 <119943324+JanTen312@users.noreply.github.com>
2023-01-29 15:50:16 +0100

    Update and rename Aaa.c to aaa.h

---
+++ Aufg-03-10/aaa.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+  int funktion() {
+    int a;
+    printf("\n\nGeben Sie eine Zahl zwischen 1-7 ein:  ");
+    scanf("%d",&a);
+    switch(a) {
+      case 1: printf("Montag\n\n");
+      break;
+      case 2: printf("Dienstag\n\n");
+      break;
+      case 3: printf("Mitwoch\n\n");
+      break;
+      case 4: printf("Donnerstag\n\n");
+      break;
+      case 5: printf("Freitag\n\n");
+      break;
+      case 6: printf("Samstag\n\n");
+      break;
+      case 7: printf("Sonntag\n\n");
+      break;
+      default: printf("Faslche Eingabe\n\n");
+      break;
+    }
+  return 0;
+  }
